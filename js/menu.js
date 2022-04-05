7a31631366cc5fb821f7eebe356ebd4d79d66868
fernando marulanda <jhonfernandomarulandaserna@gmail.com>
2022-04-05 14:52:00 -0500

    actualizado.

--- js/menu.js
+++ js/menu.js
@@ -4,9 +4,14 @@ const ham = document.querySelector(".ham");
 const menu = document.querySelector(".header");
 const barras = document.querySelectorAll(".ham span");
 const enlacesMenu = document.querySelectorAll('.menu a');
+const contact = document.querySelector("#contact");
 
-
-
+contact.addEventListener("click", ()=> {
+  menu.classList.remove("activado");
+  barras.forEach(child => {
+    child.classList.remove('animado');
+  });
+});
 
 ham.addEventListener("click", () => {
   menu.classList.toggle("activado");
